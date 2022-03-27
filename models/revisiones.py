0448e9c7e40c8619dba1abcfc3ab38b0d2565903
fadria <fadriacarrasco@gmail.com>
2022-03-27 21:08:41 +0200

    Funciones para comprobar la validez de la fecha en una revisión y que el usuario responsable tenga el rol voluntario

--- models/revisiones.py
+++ models/revisiones.py
@@ -2,6 +2,7 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 from datetime import date
+from datetime import datetime
 
 class Revisiones(models.Model):
     # NOMBRE Y DESCRIPCIÓN DEL MODELO
@@ -19,6 +20,29 @@ class Revisiones(models.Model):
 
     # ATRIBUTOS
 
-    fecha = fields.Datetime("Fecha y hora de la revisión")
-    animal = fields.Many2one('animales', store=True)
-    observaciones = fields.Html('Observaciones', sanitize=True, strip_style=False)
+    fecha = fields.Datetime("Fecha y hora de la revisión", required=True)
+    animal = fields.Many2one('animales', store=True, required=True)
+    voluntario = fields.Many2one('usuarios', store=True, required=True)
+    observaciones = fields.Html('Observaciones', sanitize=True, strip_style=False, required=True)
+
+    # FUNCIONES
+    
+    # Función usada para comprobar que la fecha no es posterior a la actual
+    @api.constrains('fecha')
+    def comprobar_fecha_revision(self):
+        # Bucle donde comprobaremos si el nuevo registro es correcto
+        for record in self:
+
+            # Si la fecha del registro es posterior a la actual lanzaremos una excepción
+            if record.fecha > datetime.now():
+                raise models.ValidationError('La fecha de publicación debe ser anterior a la actual')
+
+    # Función usada para comprobar que en el campo voluntario se ha asignado una persona con el rol voluntario
+    @api.constrains('voluntario')
+    def comprobar_voluntario(self):
+        # Bucle donde comprobaremos si el nuevo registro es correcto
+        for record in self:
+
+            # Si la fecha del registro es posterior a la actual lanzaremos una excepción
+            if record.voluntario.rol != "voluntario":
+                raise models.ValidationError('La persona que realiza la revisión debe ser un voluntario')
