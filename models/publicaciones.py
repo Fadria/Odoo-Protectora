b13c3d86259590a90ba755b1ef6125ef86469560
fadria <fadriacarrasco@gmail.com>
2022-03-27 19:26:59 +0200

    Función para comprobar la validez de la fecha de publicación en el modelo de publicaciones

--- models/publicaciones.py
+++ models/publicaciones.py
@@ -2,6 +2,7 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 from datetime import date
+from datetime import datetime
 
 class Publicaciones(models.Model):
     # NOMBRE Y DESCRIPCIÓN DEL MODELO
@@ -20,7 +21,19 @@ class Publicaciones(models.Model):
     # ATRIBUTOS
 
     imagenPortada = fields.Image("Imagen de portada")
-    fechaPublicacion = fields.Datetime("Fecha y hora de la publicación")
-    titulo = fields.Char("Título de la publicación")
-    contenido = fields.Html('Contenido', sanitize=True, strip_style=False)
-    autor = fields.Many2one("usuarios", "Autor") # Relación con el modelo de usuarios
+    fechaPublicacion = fields.Datetime("Fecha y hora de la publicación", required=True)
+    titulo = fields.Char("Título de la publicación", required=True)
+    contenido = fields.Html('Contenido', sanitize=True, strip_style=False, required=True)
+    autor = fields.Many2one("usuarios", "Autor", required=True) # Relación con el modelo de usuarios
+
+    # FUNCIONES
+
+    # Función usada para comprobar que la fecha no es posterior a la actual
+    @api.constrains('fechaPublicacion')
+    def comprobar_fecha_publicacion(self):
+        # Bucle donde comprobaremos si el nuevo registro es correcto
+        for record in self:
+
+            # Si la fecha del registro es posterior a la actual lanzaremos una excepción
+            if record.fechaPublicacion > datetime.now():
+                raise models.ValidationError('La fecha de publicación debe ser anterior a la actual')
