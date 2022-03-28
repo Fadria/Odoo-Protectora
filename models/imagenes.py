12235ce369e5372d61db8d080431185ea64464ac
fadria <fadriacarrasco@gmail.com>
2022-03-28 17:26:53 +0200

    Función para comprobar la validez de la fecha en las imágenes

--- models/imagenes.py
+++ models/imagenes.py
@@ -2,6 +2,7 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 from datetime import date
+from datetime import datetime
 
 class Imagenes(models.Model):
     # NOMBRE Y DESCRIPCIÓN DEL MODELO
@@ -18,7 +19,19 @@ class Imagenes(models.Model):
     _rec_name = 'id'    
 
     # ATRIBUTOS
+    
+    imagen = fields.Image("Imagen", required=True)
+    fecha = fields.Datetime("Fecha y hora de la revisión", required=True)
+    animal = fields.Many2one('animales', store=True, required=True)
 
-    imagen = fields.Image("Imagen")
-    fecha = fields.Datetime("Fecha y hora de la revisión")
-    animal = fields.Many2one('animales', store=True)
+    # FUNCIONES
+    
+    # Función usada para comprobar que la fecha no es posterior a la actual
+    @api.constrains('fecha')
+    def comprobar_fecha_revision(self):
+        # Bucle donde comprobaremos si el nuevo registro es correcto
+        for record in self:
+
+            # Si la fecha del registro es posterior a la actual lanzaremos una excepción
+            if record.fecha > datetime.now():
+                raise models.ValidationError('La fecha de la imagen debe ser anterior a la actual')    
